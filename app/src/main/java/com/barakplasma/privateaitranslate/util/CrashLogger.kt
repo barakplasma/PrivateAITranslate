054d7223074e8c31e7621f6c089ce7e7d00aad8a
Michael Salaverry <barakplasma@gmail.com>
2026-04-11 11:10:05 +0300

    Fix BuildConfig import in CrashLogger

--- app/src/main/java/com/barakplasma/privateaitranslate/util/CrashLogger.kt
+++ app/src/main/java/com/barakplasma/privateaitranslate/util/CrashLogger.kt
@@ -3,6 +3,7 @@ package com.barakplasma.privateaitranslate.util
 import android.content.Context
 import android.os.Build
 import android.util.Log
+import com.barakplasma.privateaitranslate.BuildConfig
 import java.io.File
 import java.io.PrintWriter
 import java.io.StringWriter
